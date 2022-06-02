fb2e702cadfcdc9fa22fd771bd2ddc73c480b338
akaikaze <akaikaze.monokuro@gmail.com>
2022-06-02 09:23:46 +0900

    複数のPPTを開くとTaskPaneが最初のwindowにしか開かない現象に対応

--- kuropptaddin/ThisAddIn.cs
+++ kuropptaddin/ThisAddIn.cs
@@ -6,22 +6,52 @@ using System.Xml.Linq;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 using Office = Microsoft.Office.Core;
 using System.Windows.Forms;
+using Microsoft.Office.Tools;
 
 namespace kuropptaddin
 {
+
+    public class TaskPaneManager
+    {
+        // base idea is 
+        // https://stackoverflow.com/questions/19160158/customtaskpane-in-excel-doesnt-appear-in-new-workbooks
+
+        static Dictionary<string, CustomTaskPane> _createdPanes = new Dictionary<string, CustomTaskPane>();
+
+        /// <summary>
+        /// Gets the taskpane by name (if exists for current excel window then returns existing instance, otherwise uses taskPaneCreatorFunc to create one). 
+        /// </summary>
+        /// <param name="taskPaneId">Some string to identify the taskpane</param>
+        /// <param name="taskPaneTitle">Display title of the taskpane</param>
+        /// <param name="taskPaneCreatorFunc">The function that will construct the taskpane if one does not already exist in the current Excel window.</param>
+        public static CustomTaskPane GetTaskPane(string taskPaneId, string taskPaneTitle, Func<UserControl> taskPaneCreatorFunc)
+        {
+            string key = string.Format("{0}({1})", taskPaneId, Globals.ThisAddIn.Application.HWND);
+            if (!_createdPanes.ContainsKey(key))
+            {
+                var pane = Globals.ThisAddIn.CustomTaskPanes.Add(taskPaneCreatorFunc(), taskPaneTitle);
+                _createdPanes[key] = pane;
+            }
+            return _createdPanes[key];
+        }
+    }
+
     public partial class ThisAddIn
     {
         public Microsoft.Office.Tools.CustomTaskPane myPane;
         public RehearsalTiming rehaCls;
         public MergeNoteForm mergeNoteForm;
+//        public static TaskPaneManager taskPaneManager = new TaskPaneManager();
+
 
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            myPane = this.CustomTaskPanes.Add(new TagEditor(), "eLearning Reharsal tools");
+//            myPane = this.CustomTaskPanes.Add(new TagEditor(), "eLearning Reharsal tools");
 
         }
         public void ShowPanel()
         {
+            myPane = TaskPaneManager.GetTaskPane("aaa", "eLearning Reharsal tools", () => new TagEditor());
             myPane.Visible = true;
             myPane.DockPosition = Office.MsoCTPDockPosition.msoCTPDockPositionBottom;
             myPane.Height = 450;
